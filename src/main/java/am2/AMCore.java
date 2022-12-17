e1f2d9e3d29d0ece13328f0d1d2a9c5fbe8608bb
Tobias Cuiper <tobias.cuiper@gmx.de>
2022-12-17 18:43:07 +0100

    changed Version to 1.4.0.010

--- src/main/java/am2/AMCore.java
+++ src/main/java/am2/AMCore.java
@@ -52,7 +52,7 @@ import net.minecraftforge.fluids.FluidRegistry;
 
 import java.io.File;
 
-@Mod(modid = "arsmagica2", modLanguage = "java", name = "Ars Magica 2", version = "1.4.0.009", dependencies = "required-after:AnimationAPI;after:lotr")
+@Mod(modid = "arsmagica2", modLanguage = "java", name = "Ars Magica 2", version = "1.4.0.010", dependencies = "required-after:AnimationAPI;after:lotr")
 public class AMCore{
 
 	@Instance(value = "arsmagica2")
